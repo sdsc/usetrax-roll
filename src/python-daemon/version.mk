39c509d9c762f35f343224861093c25d6a30527b
Trevor Cooper <tcooper@sdsc.edu>
2013-11-21 15:54:28 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/python-daemon/version.mk
+++ src/python-daemon/version.mk
@@ -1,3 +1,14 @@
-NAME    = python-daemon
-VERSION = 1.5.5
-RELEASE = 1
+NAME               = python-daemon
+VERSION            = 1.5.5
+RELEASE            = 1
+PKGROOT            = `python -c 'import sys; print sys.path[-1]'`/daemon
+
+SRC_SUBDIR         = python-daemon
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
